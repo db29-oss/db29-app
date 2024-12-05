0691ca2a945679ea9e59488c3810b249c46be602
kocoten1992 <ko_co_ten_1992@yahoo.com>
2024-12-06 00:40:35 +0700

    feat: simplify register/login process

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,5 +1,14 @@
 <div>
   <pre style="color: red">{{ __('trans.save_login_id') }}.</pre>
   <pre>login_id: {{ $user->login_id }}</pre>
-  <pre><a href="{{ route('login') }}">{{ mb_ucfirst(__('trans.login')) }}</a></pre>
+
+  <form method="POST" action="{{ route('login') }}">
+    <input type="hidden" id="login_id" name="login_id" value ="{{ $user->login_id }}" required>
+
+    @csrf
+
+    <div>
+      <button type="submit">{{ __('trans.login') }}</button>
+    </div>
+  </form>
 </div>
