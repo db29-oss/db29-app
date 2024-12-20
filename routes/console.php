ac49c5de9f06b0d908c91d031b54eb92308cc8b7
kocoten1992 <ko_co_ten_1992@yahoo.com>
2024-12-20 16:31:07 +0700

    feat+fix: prevent scheduler lock forever

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:traffic-router-maintain')->onOneServer()->withoutOverlapping()->everyMinute();
+Schedule::command('app:traffic-router-maintain')->onOneServer()->withoutOverlapping(10)->everyMinute();
 
-Schedule::command('app:take-credit')->onOneServer()->withoutOverlapping()->hourly();
-Schedule::command('app:turn-off-free-instance')->onOneServer()->withoutOverlapping()->hourly();
+Schedule::command('app:take-credit')->onOneServer()->withoutOverlapping(10)->hourly();
+Schedule::command('app:turn-off-free-instance')->onOneServer()->withoutOverlapping(10)->hourly();
 
-Schedule::command('app:instance-cleanup')->onOneServer()->withoutOverlapping()->daily();
-Schedule::command('app:user-cleanup')->onOneServer()->withoutOverlapping()->daily();
+Schedule::command('app:instance-cleanup')->onOneServer()->withoutOverlapping(10)->daily();
+Schedule::command('app:user-cleanup')->onOneServer()->withoutOverlapping(10)->daily();
