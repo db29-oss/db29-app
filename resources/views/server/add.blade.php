f7bc7947d98492a1eacb72668c6431a355f1b2c1
kocoten1992 <ko_co_ten_1992@yahoo.com>
2025-01-21 15:34:11 +0700

    feat: keep old input add user own server

--- resources/views/server/add.blade.php
+++ resources/views/server/add.blade.php
@@ -11,28 +11,28 @@
     @csrf
 
     <label class="select-none" for="ssh_address">ssh address*</label><br>
-    <input type="text" name="ssh_address" value=""><br>
+    <input type="text" name="ssh_address" value="{{ old('ssh_address') }}"><br>
     @error('ssh_address')
     <div style="display: inline; color: red">{{ $message }}</div>
     @enderror
     <br>
 
     <label class="select-none" for="ssh_port">ssh port*</label><br>
-    <input type="text" name="ssh_port" value=""><br>
+    <input type="text" name="ssh_port" value="{{ old('ssh_port') }}"><br>
     @error('ssh_port')
     <div style="display: inline; color: red">{{ $message }}</div>
     @enderror
     <br>
 
     <label class="select-none" for="ssh_privatekey">ssh privatekey*</label><br>
-    <textarea type="text" name="ssh_privatekey" value=""></textarea><br>
+    <textarea type="text" name="ssh_privatekey">{{ old('ssh_privatekey') }}</textarea><br>
     @error('ssh_privatekey')
     <div style="display: inline; color: red">{{ $message }}</div>
     @enderror
     <br>
 
     <label class="select-none" for="storage_path">storage path</label><br>
-    <input type="text" name="storage_path" value=""
+    <input type="text" name="storage_path" value="{{ old('storage_path') }}"
       placeholder="{{ __('trans.default') }} (/opt/)"
     ><br>
     @error('storage_path')
