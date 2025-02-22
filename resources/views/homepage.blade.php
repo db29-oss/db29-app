ae0616ca44fceca6c4f103a7a97c6a6316651560
kocoten1992 <ko_co_ten_1992@yahoo.com>
2025-02-23 03:37:00 +0700

    feat: add youtube introduction on homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,4 +9,7 @@
   <div class="mt-4">
     {!! __('trans.checkout_social_link') !!}
   </div>
+  <br>
+  <br>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/nR7GX93MAzA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </body>
